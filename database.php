a283c0ad664a2be49f06974a145db5cfd2fa8c21
Tiến Sĩ Hóa <Tin S Ha>
2016-06-06 19:30:13 +0700

    Database Huemobi

--- database.php
+++ database.php
@@ -1,60 +1,40 @@
 <?php
-	class Data
-	{
-		private static $servername = "localhost";
-		private static $username = "root";
-		private static $password = "";
-		private static $databaseName = "datahuemobi";
+/**
+* 
+*/
+class Database
+{	
+	private static $servername = "localhost";
+	private static $username = "root";
+	private static $password = "";
+	private static $dbname = "test";
+	private static $conn = null;
 
-		private static $conn = null;
+	public function __construct(){
+		exit('Init function is not allowed');
+	}
 
-		public function __construct() {
-			exit('Init function is not allowed');
-		}
-		
-		public static function connect()
-		{
-		   // One connection through whole application
-	       if ( null == self::$conn )
-	       {      
-		        self::$conn = mysqli_connect(self::$servername, self::$username, self::$password, self::$databaseName);
-		        self::$conn->set_charset("utf8");
-		        if (!self::$conn) {
-				    die("Connection failed: " . mysqli_connect_error());
-				}
-	       } 
-	       return self::$conn;
-		}
-		
-		public static function disconnect()
-		{
-			mysqli_close(self::$conn);
-		} 
+	public static function connect()
+	{	// Create connection
 
-		public static function selectTable($conn,$nametable,$id,$dataid)//lấy dữ liệu từ $nametable có điều kiện hoặc không, nếu có điều kiện thì theo $id có dữ liệu kiểm tra là $dataid, ngược lại không có diều kiện các biến chuổi rổng "" xảy ra lổi nếu có kết quả null -> dùng để kiểm tra mã id
+		if(null == self::$conn)
 		{
-			if ($conn!=null&&$nametable!=null) {
-				if($id!="")
-				{
-					$sql="SELECT * FROM ".$nametable." WHERE ".$id."='$dataid'";
-				}
-				else
-				{
-					$sql="SELECT * FROM ".$nametable;
-				}
-				$results=mysqli_query($conn,$sql);
-				if($results!=null){
-					if(mysqli_num_rows($results)>0){
-						return $results;
-					}
-					else
-						return null;
-				}
-				else
-					return null;
+
+			self::$conn = new mysqli(self::$servername, self::$username, self::$password, self::$dbname);
+			self::$conn->set_charset("utf8");
+			if(self::$conn->connect_error)
+			{
+				die("Connection failed: " . self::$conn->connect_error);
 			}
-			else
-				return null;
+			return self::$conn;
+
 		}
 	}
+	public static function disconnect()
+	{
+
+		self::$conn->close();
+	}
+
+}
 ?>
