a86d53d560f592b491c6985c5f13f983e080f31e
tuancules <tuancules>
2016-06-15 19:40:34 +0700

    giao diện trang chủ #tuấn

---
+++ trangchu/index.php
@@ -0,0 +1,327 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>ĐIỆN THOẠI HUẾ</title>
+ <link rel="stylesheet" href="CSS/reset.css">
+ <link rel="stylesheet" href="CSS/style.css">
+</head>
+
+<body>
+	<div id="contener">
+    
+    	<div id="header">
+            <div class="headermenu">
+            	<div class="logo">
+                	<a href="#"><img src="img/banner/thegioididong.png" /></a>
+                    <form action="#" method="POST" name="seach">
+                        <input type="text" name="seach" class="input" placeholder="Bạn tìm gì ..." />
+                        <button type="submit" ><img src="img/banner/seach.png"</button>
+                    </form>
+                </div><!--logo-->
+                <div class="list">
+                	<ul class="list1">
+                    	<li>
+                            <!-- <ul class="menu-con">
+                                <li> <a href="#">APPLE</a></li>
+                                <li> <a href="#">SAMSUNG</a></li>
+                                <li> <a href="#">NOKIA</a></li>
+                                <li> <a href="#">SONY</a></li>
+                                <li> <a href="#">LG</a></li>
+                                <li> <a href="#">HTC</a></li>
+                                <li> <a href="#">OPPLE</a></li>
+                                <li> <a href="#">ASUS</a></li>
+                                <
+                            </ul> -->
+                        	<a href="#"><img src="img/banner/005.png" alt="Điện thoại" ><span>Điện thoại</span></a>
+                        </li>
+                        <li>
+                            <!-- <ul class="menu-con">
+                                <li> <a href="#">APPLE</a></li>
+                                <li> <a href="#">SAMSUNG</a></li>
+                                <li> <a href="#">ACER</a></li>
+                                <li> <a href="#">DELL</a></li>
+                                <li> <a href="#">WING</a></li>
+                                <li> <a href="#">MASSTER</a></li>
+                                <li> <a href="#">LNOVO</a></li>
+                                <li> <a href="#">ASUS</a></li>
+                            </ul> -->
+                        	<a href="#"><img src="img/banner/036.png" alt="TABLE" /> <span>Table</span> </a>
+                        </li>
+                        <li>
+                           <!--  <ul class="menu-con">
+                                <li> <a href="#">APPLE</a></li>
+                                <li> <a href="#">HP</a></li>
+                                <li> <a href="#">ACER</a></li>
+                                <li> <a href="#">DELL</a></li>
+                                <li> <a href="#">TOSHIBA</a></li>
+                                <li> <a href="#">ASUS</a></li>
+                            </ul> -->
+                        	<a href="#"> <img src="img/banner/033.png"alt="laptop" /><span>Latop</span></a>
+                        </li>
+                        <li>
+                           <!--  <ul class="menu-con">
+                                <li> <a href="#">SẠC</a></li>
+                                <li> <a href="#">LOA</a></li>
+                                <li> <a href="#">TÚI XÁCH</a></li>
+                                <li> <a href="#">BỘ NHỚ</a></li>
+                                <li> <a href="#">KÍNH</a></li>
+                            </ul> -->
+                        	<a href="#" > <img src="img/banner/049.png" alt="phụ kiện" /> <span>phụ kiện</span> </a>
+                        </li>
+                        <li>
+                        	<a href="#" ><img src="img/banner/067.png" alt="sim, thẻ"/> <span>sim,thẻ</span></a>
+                        </li>
+                        <li> 
+                        	<a href="#"><img src="img/banner/040.png" alt="Công nghệ"/> <span>công nghệ</span></a>
+                            </li>
+                        <li>
+                        	 <a href="#"><img src="img/banner/21.png" alt=hỏi,đáp""/><span>hỏi đáp</span></a>
+                         </li>
+                        <li>
+                        	 <a href="#"><img src="img/banner/145.png"/><span>game,APP</span></a>
+                         </li>
+                         <li>
+                             <a href="#"><img src="img/banner/qt.png"/><span>quà tặng</span></a>
+                         </li>
+                    </ul>
+                </div> <!--list tùy chọn-->
+            </div>
+            <div>
+                <div></div>
+                <div></div>   
+            </div><!--JAVA scrip-->
+        </div>
+        <!--....................................header.........................................................-->
+        
+        <!--<div id="conten">
+            <div class="sanpham">
+                <div class="sanpham1">
+                    <div class="sanpham1a">
+                        <img src="img/conten/J5.PNG" alt="J5" />
+                        <h3>Galaxy J5</h3>
+                        <p>Camera thủ lĩnh bóng đêm</p>
+                        <h4>Galaxy J5</h4>
+                        <p>5.000.000đ</p>
+                    </div>
+                    <div class="sanpham1b">
+                        <img src="img/conten/J5.PNG" alt="J5" />
+                        <h3>Galaxy J5</h3>
+                        <p>Camera thủ lĩnh bóng đêm</p>
+                        <h4>Galaxy J5</h4>
+                        <p>5.000.000đ</p>
+                    </div> 
+                </div>
+                <div class="sanpham2">
+                    <div class="sanpham2a">
+                        <img src="img/conten/J5.PNG" alt="J5" />
+                        <h3>Galaxy J5</h3>
+                        <p>Camera thủ lĩnh bóng đêm</p>
+                        <h4>Galaxy J5</h4>
+                        <p>5.000.000đ</p>
+                    </div>
+                    <div class="sanpham2b">
+                        <img src="img/conten/J5.PNG" alt="J5" />
+                        <h3>Galaxy J5</h3>
+                        <p>Camera thủ lĩnh bóng đêm</p>
+                        <h4>Galaxy J5</h4>
+                        <p>5.000.000đ</p>
+                    </div>
+                </div>
+                <div class="sanpham3">
+                    <div class="sanpham3a">
+                        <img src="img/conten/J5.PNG" alt="J5" />
+                        <h3>Galaxy J5</h3>
+                        <p>Camera thủ lĩnh bóng đêm</p>
+                        <h4>Galaxy J5</h4>
+                        <p>5.000.000đ</p>
+                    </div>
+                    <div class="sanpham3b">
+                        <img src="img/conten/J5.PNG" alt="J5" />
+                        <h3>Galaxy J5</h3>
+                        <p>Camera thủ lĩnh bóng đêm</p>
+                        <h4>Galaxy J5</h4>
+                        <p>5.000.000đ</p>
+                    </div>
+                    <div class="sanpham3c">
+                        <img src="img/conten/J5.PNG" alt="J5" />
+                        <h3>Galaxy J5</h3>
+                        <p>Camera thủ lĩnh bóng đêm</p>
+                        <h4>Galaxy J5</h4>
+                        <p>5.000.000đ</p>
+                    </div>
+                </div>
+                <div class="sanpham4">
+                    <div class="sanpham4a">
+                        <img src="img/conten/J5.PNG" alt="J5" />
+                        <h3>Galaxy J5</h3>
+                        <p>Camera thủ lĩnh bóng đêm</p>
+                        <h4>Galaxy J5</h4>
+                        <p>5.000.000đ</p>
+                    </div>
+                    <div class="sanpham4b">
+                        <img src="img/conten/J5.PNG" alt="J5" />
+                        <h3>Galaxy J5</h3>
+                        <p>Camera thủ lĩnh bóng đêm</p>
+                        <h4>Galaxy J5</h4>
+                        <p>5.000.000đ</p>
+                    </div>
+                    <div class="sanpham4c">
+                        <img src="img/conten/J5.PNG" alt="J5" />
+                        <h3>Galaxy J5</h3>
+                        <p>Camera thủ lĩnh bóng đêm</p>
+                        <h4>Galaxy J5</h4>
+                        <p>5.000.000đ</p>
+                    </div>
+                </div>
+            </div>--><!--sản phẩm-->
+            <!--<div class="tintuc">
+                <div class="tieude">
+                    <a class="left" href="#"><h3>TIN CÔNG NGHỆ</h3></a>
+                    <a class="leftleft" href="#">xem thêm </a>
+                </div>
+                <div class="noidung">
+                    <a href="#" class="noidungleft">
+                        <img src="img/conten/samsung.png" alt="SamSung" />
+                        <h3>Samsung dẫn đầu về công nghệ màn hình uốn dẻo</h3>
+                    </a>
+                    <a href="#" class="noidungleft">
+                        <img src="img/conten/samsung.png" alt="SamSung" />
+                        <h3>Samsung dẫn đầu về công nghệ màn hình uốn dẻo</h3>
+                    </a>
+                    <a href="#" class="noidungleft">
+                        <img src="img/conten/samsung.png" alt="SamSung" />
+                        <h3>Samsung dẫn đầu về công nghệ màn hình uốn dẻo</h3>
+                    </a>
+                    <a href="#" class="noidungleft">
+                        <img src="img/conten/samsung.png" alt="SamSung" />
+                        <h3>Samsung dẫn đầu về công nghệ màn hình uốn dẻo</h3>
+                    </a>
+                    <a href="#" class="noidungleft">
+                        <img src="img/conten/samsung.png" alt="SamSung" />
+                        <h3>Samsung dẫn đầu về công nghệ màn hình uốn dẻo</h3>
+                    </a>
+                    <a href="#" class="noidungleft">
+                        <img src="img/conten/samsung.png" alt="SamSung" />
+                        <h3>Samsung dẫn đầu về công nghệ màn hình uốn dẻo</h3>
+                    </a>
+                    <a href="#" class="noidungleft">
+                        <img src="img/conten/samsung.png" alt="SamSung" />
+                        <h3>Samsung dẫn đầu về công nghệ màn hình uốn dẻo</h3>
+                    </a>
+                    <a href="#" class="noidungleft">
+                        <img src="img/conten/samsung.png" alt="SamSung" />
+                        <h3>Samsung dẫn đầu về công nghệ màn hình uốn dẻo</h3>
+                    </a>
+                    <a href="#" class="noidungleft">
+                        <img src="img/conten/samsung.png" alt="SamSung" />
+                        <h3>Samsung dẫn đầu về công nghệ màn hình uốn dẻo</h3>
+                    </a>
+                    <a href="#" class="noidungleft">
+                        <img src="img/conten/samsung.png" alt="SamSung" />
+                        <h3>Samsung dẫn đầu về công nghệ màn hình uốn dẻo</h3>
+                    </a>
+                    <a href="#" class="noidungleft">
+                        <img src="img/conten/samsung.png" alt="SamSung" />
+                        <h3>Samsung dẫn đầu về công nghệ màn hình uốn dẻo</h3>
+                    </a>
+                    <a href="#" class="noidungleft">
+                        <img src="img/conten/samsung.png" alt="SamSung" />
+                        <h3>Samsung dẫn đầu về công nghệ màn hình uốn dẻo</h3>
+                    </a>
+            </div>--><!--tin tức-->
+            <!--<div class="button">
+                <div>
+                    <a href="#">Khuyến mãi tháng 6</a>
+                    <a href="#">Điện thoại (184)</a>
+                    <a href="#">Máy tính bảng (68)</a>
+                    <a href="#">Laptop (71)</a>
+                    <a href="#">Phụ kiện (1000)</a>
+                    <a href="#">máy đã sử dụng (11220)</a>
+                </div>
+                <div>
+                    <span>tìm kiếm nhiều:</span>
+                    <a href="#">• Mobell M328</a>
+                    <a href="#">• Mobiistar Buddy</a>
+                    <a href="#">• Ốp lưng iPhone 6s</a>
+                    <a href="#">• Phụ kiện iPhone 6s Plus</a>
+                    <a href="#">• Galaxy J1 2016</a>
+                    <a href="#">• Samsung Gear</a>
+                </div>
+            </div>
+        </div>--><!--san pham giới thiệu-->
+        <!--..................................................conten..............................................-->
+         <div id="footer">
+            <div class="hotro">
+                <div class="thongtin" id="hotro1">
+                    <ul>
+                        <li>
+                            <H3>HỖ TRỢ KHÁCH HÀNG</H3>
+                        </li>
+                        <li>  
+                            <a href="#">Tìm hiểu về mua trả góp</a>
+                         </li>
+                         <li>  
+                            <a href="#">Tìm trung tâm bảo hành hãng</a>
+                         </li>
+                         <li>  
+                            <a href="#">Chính sách đổi trả</a>
+                         </li>
+                         <li>  
+                            <a href="#">Hướng dẫn đổi trả</a>
+                         </li>
+                         <li>  
+                            <a href="#">Giao hàng và đổi trả</a>
+                         </li>
+                         <li>  
+                            <a href="#">Xem thêm</a>
+                         </li>
+                    </ul>
+                </div>
+                <div class="thongtin" id="gioithieu">
+                    <ul>
+                        <li>
+                            <H3>DIENTHOAIHUE.COM</H3>
+                        </li>
+                        <li>  
+                            <a href="#">Giới thiệu công ty</a>
+                         </li>
+                         <li>  
+                            <a href="#">Tuyển dụng</a>
+                         </li>
+                         <li>  
+                            <a href="#">Chính sách đổi trả</a>
+                         </li>
+                         <li>  
+                            <a href="#">Bảo hành</a>
+                         </li>   
+                    </ul>
+                </div>
+                <div class="thongtin" id="thacmac">
+                    <H3>CÓ THẮC MẮC</H3>
+                    <p>Bán hàng (7:30 - 22:00)</p>
+                    <p><b>18001060</b>Hoặc<b>(08)38.102.102</b></p>
+                    <p>Góp ý, khiếu nại (8:00 - 21:30)<b>1800.1062</b></p>
+                    <p>Bảo hành sản phẩm (8:00 - 21:00)<b>1800.1064</b></p>
+                </div>
+        </div> <!--Hỗ trợ-->
+        <div class="address">
+            <div class="address1" id="ad1">
+                <p>Công ty cổ phần Điện thoại Huế. Chính sách sử dụng web văn phòng: <a href="#">11 Hùng Vương - tp Huế</a>
+                 </p>
+            </div>
+            <div>
+                
+            </div>
+            <div class="address1" id="ad2">
+                <p>Bạn có thể thanh toán với</p>
+                <a href="#">Tiền mặt</a>
+                <a href="#">ATM</a>
+            </div>
+        </div><!--Địa chỉ-->
+        
+        <!--..........................................footer....................................................-->
+        
+    </div><!--contener-->
+</body>
+</html>
