d365f400e14c11b6d09df89aad4cb35877654a00
tuancules <tuancules>
2016-06-20 17:30:24 +0700

    no message

---
+++ trangchu/khuyenmai.php
@@ -0,0 +1,150 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>ĐIỆN THOẠI HUẾ</title>
+ <link rel="stylesheet" href="CSS/reset.css">
+ <link rel="stylesheet" href="CSS/khuyenmai.css">
+</head>
+
+<body>
+    <div id="contener">
+    
+        <div id="header">
+            <div class="headermenu">
+                <div class="logo">
+                    <a href="#"><img src="img/banner/thegioididong.png" /></a>
+                    <form action="#" method="POST" name="seach">
+                        <input type="text" name="seach" class="input" placeholder="Bạn tìm gì ..." />
+                        <button type="submit" ><img src="img/banner/seach.png"/></button>
+                    </form>
+                </div><!--logo-->
+                <div class="list">
+                    <ul class="list1">
+                        <li>
+                            <a href="#"><img src="img/banner/005.png" alt="Điện thoại" ><span>Điện thoại</span></a>
+                        </li>
+                        <li>
+                            <a href="#"><img src="img/banner/036.png" alt="TABLE" /> <span>Table</span> </a>
+                        </li>
+                        <li>
+                            <a href="#"> <img src="img/banner/033.png"alt="laptop" /><span>Latop</span></a>
+                        </li>
+                        <li>
+                            <a href="#" > <img src="img/banner/049.png" alt="phụ kiện" /> <span>phụ kiện</span> </a>
+                        </li>
+                        <li>
+                            <a href="#" ><img src="img/banner/067.png" alt="sim, thẻ"/> <span>sim,thẻ</span></a>
+                        </li>
+                        <li> 
+                            <a href="#"><img src="img/banner/040.png" alt="Công nghệ"/> <span>công nghệ</span></a>
+                            </li>
+                        <li>
+                             <a href="#"><img src="img/banner/21.png" alt=hỏi,đáp""/><span>hỏi đáp</span></a>
+                         </li>
+                        <li>
+                             <a href="#"><img src="img/banner/145.png"/><span>game,APP</span></a>
+                         </li>
+                         <li>
+                             <a href="#"><img src="img/banner/qt.png"/><span>quà tặng</span></a>
+                         </li>
+                    </ul>
+                </div> <!--list tùy chọn-->
+            </div>
+        </div>
+        <!--....................................header.........................................................-->
+        
+        <div class="conten">
+            <img src="img/banner/khuyenmai.png" alt="khuyến mãi"/>
+            <div class="conment">
+                <div class="submitcomment">
+                    <form action="#" method="post" name="comment"> 
+                    <span>Tên của bạn</span>
+                    <input type="text" name="yourname" placeholder="hãy nhập tên của bạn" />
+                    <span>Địa chỉ email:</span>
+                    <input type="text" name="email" placeholder="nhập địa chỉ email"/>
+                    <textarea name="comment" id="comment" cols="50" rows="2" tabindex="4"></textarea>
+                    <input type="submit" name="submit" value="Gửi"/>
+                    </form>
+                </div>
+                <div class="printcomment">
+                    <p>Họ và tên</p>
+                    <p>bình luận</p>
+                </div>
+            </div><!--bình luận-->
+        </div><!--content-->
+        <!--..................................................conten..............................................-->
+         <div id="footer">
+            <div class="hotro">
+                <div class="thongtin" id="hotro1">
+                    <ul>
+                        <li>
+                            <H3>HỖ TRỢ KHÁCH HÀNG</H3>
+                        </li>
+                        <li>  
+                            <a href="#">Tìm hiểu về mua trả góp</a>
+                         </li>
+                         <li>  
+                            <a href="#">Tìm trung tâm bảo hành hãng</a>
+                         </li>
+                         <li>  
+                            <a href="#">Chính sách đổi trả</a>
+                         </li>
+                         <li>  
+                            <a href="#">Hướng dẫn đổi trả</a>
+                         </li>
+                         <li>  
+                            <a href="#">Giao hàng và đổi trả</a>
+                         </li>
+                         <li>  
+                            <a href="#">Xem thêm</a>
+                         </li>
+                    </ul>
+                </div>
+                <div class="thongtin" id="gioithieu">
+                    <ul>
+                        <li>
+                            <H3>DIENTHOAIHUE.COM</H3>
+                        </li>
+                        <li>  
+                            <a href="#">Giới thiệu công ty</a>
+                         </li>
+                         <li>  
+                            <a href="#">Tuyển dụng</a>
+                         </li>
+                         <li>  
+                            <a href="#">Chính sách đổi trả</a>
+                         </li>
+                         <li>  
+                            <a href="#">Bảo hành</a>
+                         </li>   
+                    </ul>
+                </div>
+                <div class="thongtin" id="thacmac">
+                    <H3>CÓ THẮC MẮC</H3>
+                    <p>Bán hàng (7:30 - 22:00)</p>
+                    <p><b>18001060</b>Hoặc<b>(08)38.102.102</b></p>
+                    <p>Góp ý, khiếu nại (8:00 - 21:30)<b>1800.1062</b></p>
+                    <p>Bảo hành sản phẩm (8:00 - 21:00)<b>1800.1064</b></p>
+                </div>
+            </div> <!--Hỗ trợ-->
+            <div class="address">
+            <div class="address1" id="ad1">
+                <p>Công ty cổ phần Điện thoại Huế. Chính sách sử dụng web văn phòng: <a href="#">11 Hùng Vương - tp Huế</a>
+                 </p>
+            </div>
+            <div>
+                
+            </div>
+            <div class="address1" id="ad2">
+                <p>Bạn có thể thanh toán với</p>
+                <a href="#">Tiền mặt</a>
+                <a href="#">ATM</a>
+            </div>
+            </div><!--Địa chỉ-->
+         </div>
+        <!--..........................................footer....................................................-->
+        
+    </div><!--contener-->
+</body>
+</html>
