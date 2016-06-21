77af5463972f737208ce2dd44fc9ffc0b6a1e9af
Tiến Sĩ Hóa <Tin S Ha>
2016-06-21 15:01:03 +0700

    chỉnh lại header.php, style.css theo chuẩn tgdd

--- mobileIndex.php
+++ mobileIndex.php
@@ -1,34 +1,115 @@
 <!DOCTYPE html>
-<html>
+<html lang="vi">
 <head>
 	<title>Điện thoại, smartphone mới, giá tốt, có khuyến mãi - Huemobile.com</title>
 	<meta charset="utf-8">
 	<link rel="shortcut icon" href="assets/ico/logohuemobi16x16.png">
 	<link rel="stylesheet" href="assets/css/mobileIndex.css">
 	<link rel="stylesheet" href="assets/css/style.css">
+	<link rel="stylesheet" href="assets/css/reset.css">
 </head>
 <body>
-	<div id="container">
-		<div id="header">
-			<?php include 'header.php'; ?>
-			<div class="breadandfilter">
-				<ul>
-					<li class="breadcrumb">
-					<a href="index.php" title="Trang chủ"><img class="icon_home" src="assets/ico/icon_home.png" /></a>
-					<span>›</span>
-					</li>
+	<section>
+		<div class="breadandfilter">
+			<ul class="breadcrumb">
+        		<li typeof="v:Breadcrumb">
+        			<a href="index.php" title="Trang chủ">
+        				<img class="icon_home" src="assets/ico/icon_home.png"/>
+        			</a>
+        			<span>›</span>
+        		</li>
 
-					<li class="breadcrumb">
-                	<h1><a href="#" title="Điện thoại di động">185 Điện thoại di động</a> 
-                	<span>›</span>
-                	</h1>
-            		</li>
-				</ul>
-				
-			</div>
-			
-		</div>
-		
-	</div>
+       			<li typeof="v:Breadcrumb">
+            		<h1>
+            			<a href="dtdd" title="Điện thoại di động">178 Điện thoại di động</a> 
+            			<span>›</span>
+            		</h1>
+        		</li>
+    		</ul>
+
+    		<ul class="filter">
+    			<li>
+    				<label>Từ 1-3 triệu
+    					<span class="arrdown"></span>
+    					<div class="dropfilter">
+				            <a href="/dtdd" title=""><i></i>Tất cả</a>
+				            <a href="/dtdd?p=duoi-1-trieu" title="">Dưới 1 triệu</a>
+				            <a href="/dtdd" title=""><i class="icontgdd-checkall"></i>Từ 1 - 3 triệu</a>
+				            <a href="/dtdd?p=tu-3-7-trieu" title="">Từ 3 - 7 triệu</a>
+				            <a href="/dtdd?p=tu-7-10-trieu" title="">Từ 7 - 10 triệu</a>
+				            <a href="/dtdd?p=tu-10-15-trieu" title="">Từ 10 - 15 triệu</a>
+				            <a href="/dtdd?p=tren-15-trieu" title="">Trên 15 triệu</a>
+				    	</div>
+				    </label>
+    			</li> <!-- 1- 3 triệu -->
+
+    			<li>
+	                <label>Hãng sản xuất
+	    				<span class="arrdown"></span>            
+	    				<div class="twocolum">
+	                		<a class="all" href="#" title=""><i class="icontgdd-checkall"></i>Tất cả</a>
+	            			<aside class="left">
+	                    		<a href="/dtdd-samsung?p=tu-1-3-trieu" title="">Samsung</a>
+	                    		<a href="/dtdd-nokia-microsoft?p=tu-1-3-trieu" title="">Nokia - Microsoft</a>
+	                    		<a href="/dtdd-apple-iphone?p=tu-1-3-trieu" title="">Apple (iPhone)</a>
+			                    <a href="/dtdd-sony?p=tu-1-3-trieu" title="">Sony</a>
+			                    <a href="/dtdd-oppo?p=tu-1-3-trieu" title="">OPPO</a>
+			                    <a href="/dtdd-htc?p=tu-1-3-trieu" title="">HTC</a>
+			                    <a href="/dtdd-motorola?p=tu-1-3-trieu" title="">Motorola</a>
+			                    <a href="/dtdd-asus-zenfone?p=tu-1-3-trieu" title="">Asus (Zenfone)</a>
+			                    <a href="/dtdd-huawei?p=tu-1-3-trieu" title="">Huawei</a>
+	            			</aside>
+
+	            			<aside class="right">
+			                    <a href="/dtdd-lg?p=tu-1-3-trieu" title="">LG</a>
+			                    <a href="/dtdd-mobiistar?p=tu-1-3-trieu" title="">Mobiistar</a>
+			                    <a href="/dtdd-lenovo?p=tu-1-3-trieu" title="">Lenovo</a>
+			                    <a href="/dtdd-philips?p=tu-1-3-trieu" title="">Philips</a>
+			                    <a href="/dtdd-zte?p=tu-1-3-trieu" title="">ZTE</a>
+			                    <a href="/dtdd-mobell?p=tu-1-3-trieu" title="">Mobell</a>
+			                    <a href="/dtdd-obi-worldphone?p=tu-1-3-trieu" title="">Obi Worldphone</a>
+			                    <a href="/dtdd-wing?p=tu-1-3-trieu" title="">Wing</a>
+			                    <a href="/dtdd-q-mobile?p=tu-1-3-trieu" title="">Q-Mobile</a>
+			                    <a href="/dtdd-wiko?p=tu-1-3-trieu" title="">WIKO</a>
+			            	</aside>
+	        			</div>
+					</label>
+        		</li> <!-- hãng sx -->	
+
+        		<li>
+                    <a href="javascript:void(0)" class="moresearch">Tìm nâng cao
+                    	<span class="arrdown"></span>
+                    </a>
+            	</li> <!-- Tìm nâng cao -->
+
+            	<li class="newprod">
+				    <a href="/dtdd?n=moi-ra-mat&amp;p=tu-1-3-trieu" class="">
+				        <span></span>Mới</a>
+				</li>
+
+				<li class="newprod">
+				    <a href="/dtdd?q=tra-gop-0-phan-tram&amp;p=tu-1-3-trieu" class="">
+				        <span></span>Trả góp 0-1%</a>
+				</li>
+
+				<li class="clearfilter" onclick="javascript:location.href='dtdd'">
+					<i class="icontgdd-clearfilter"></i>Xóa điều kiện lọc <!-- img -->
+				</li>
+
+				<li class="newprod apple">
+			        <a href="/apple" target="_blank">
+			            <i class="icontgdd-ios"></i>Góc iFan</a>
+			    </li>
+
+    		</ul>
+
+		</div>	<!-- bread and filter -->
+	</section> <!-- section -->
+	<?php include 'header.php'; ?>
+	<!-- header -->
+	<div id="footer">
+		<?php include 'footer.php'; ?>
+	</div> <!-- footer -->
+	
 </body>
 </html>
